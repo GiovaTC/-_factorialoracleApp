55545977ad24abc46cda35a8f22cae06a77d3afa
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-08 11:06:10 -0500

    commit - factorial.java :.

---
+++ src/com/factorial/Factorial.java
@@ -0,0 +1,16 @@
+package com.factorial;
+
+public class Factorial {
+
+    public static long calcular(int numero) {
+        if (numero < 0) {
+            throw new IllegalArgumentException("el numero debe ser positivo .");
+        }
+
+        long resultado = 1;
+        for (int i = 1; i <= numero; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
